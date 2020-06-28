651a16b54cd4aeb6eba534b90a00d4c5d9801c28
ShadowWalker98 <44528434+ShadowWalker98@users.noreply.github.com>
2020-06-28 20:38:17 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    /*
+    int someArray[10] = {3, 6, 8, 9, 23, 45, 17, 49, 91, 34};
+
+    int *pLocation0 = someArray;
+    for(int i = 0; i < 10; i++ ){
+        //cout << someArray + i << " = " << *(someArray + i) << endl;
+        cout << pLocation0 << " = " << *pLocation0 << endl;
+        pLocation0++;
+    }
+
+//    cout << "pLocation6: " << (int) pLocation6 << endl;
+//    cout << "pLocation0: " << (int) pLocation0 << endl;
+//    cout << "Difference: " << pLocation6 - pLocation0 << endl;
+    //system("pause");
+
+     */
+
+    /*
+    char someString[] = "Hello!";
+    char *pSomeString = someString;
+
+    cout << "pSomeString: " << pSomeString << endl;
+
+    char *pLocation0 = &someString[0];
+    char *pLocation3 = &someString[3];
+
+    cout << "pLocation0: " << (int)pLocation0 << endl;
+    cout << "pLocation3: " << (int)pLocation3 << endl;
+    cout << "Difference: " << pLocation3 - pLocation0 << endl;
+    */
+    return 0;
+}
